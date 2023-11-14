3030a5dbcfafac5924881f8e4979cc7c65fa8a94
suvanbanerjee <banerjeesuvan@gmail.com>
2023-11-14 18:05:48 +0530

    added book cypher

---
+++ Simple Cypher/Transportation/book.py
@@ -0,0 +1,14 @@
+"""
+The Book Cypher method involves using a specific page from a book as the key to decode a message.
+Each word in the message corresponds to a specific word on the key page, and the decoded message is
+formed by concatenating the corresponding words from the key page.
+
+as book cypher is a physical thing we are not going to implement it here
+PEACE 
+"""
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
